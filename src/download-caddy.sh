ff23c64357f670bb1b7f18bb5861662ba4c8bc18
m7kc <69662867+m7kc@users.noreply.github.com>
2020-09-07 13:45:19 +0000

    Update download-caddy.sh

--- src/download-caddy.sh
+++ src/download-caddy.sh
@@ -5,7 +5,7 @@ _download_caddy_file() {
 	if [[ ! ${caddy_arch} ]]; then
 		echo -e "$red 获取 Caddy 下载参数失败！$none" && exit 1
 	fi
-	local caddy_download_link="https://github.com/mholt/caddy/releases/download/v0.11.0/caddy_v0.11.0_linux_${caddy_arch}.tar.gz"
+	local caddy_download_link="https://github.com/caddyserver/caddy/releases/download/v1.0.4/caddy_v1.0.4_linux_${caddy_arch}.tar.gz"
 
 	mkdir -p $caddy_tmp
 
